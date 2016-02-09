b87036bf7e4bc4087d1cfabc7a14305c01452444
Ron Rademaker <ron@connectholland.nl>
2016-02-09 08:04:18 +0100

    Added basic controller implementation

--- src/ConnectHolland/HealthCheckBundle/Controller/HealthCheckController.php
+++ src/ConnectHolland/HealthCheckBundle/Controller/HealthCheckController.php
@@ -2,12 +2,101 @@
 
 namespace ConnectHolland\HealthCheckBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ConnectHolland\HealthCheckBundle\Health\Controller\ControllerInterface;
+use ConnectHolland\HealthCheckBundle\Health\Runner\RunnerInterface;
+use ConnectHolland\HealthCheckBundle\Health\Suite\SuiteInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\EngineInterface;
 
-class DefaultController extends Controller
+/**
+ * Controller to perform the health checks
+ *
+ * @author Ron Rademaker
+ */
+class HealthCheckController implements ControllerInterface
 {
-    public function indexAction()
+    /**
+     * Health check runner
+     *
+     * @var RunnerInterface
+     */
+    private $runner;
+
+    /**
+     * Templating engine
+     *
+     * @var EngineInterface
+     */
+    private $templating;
+
+
+    /**
+     * Array of tested $suites
+     *
+     * @var array
+     */
+    private $suites;
+
+    /**
+     * Creates a new HealthCheckController
+     *
+     * @param RunnerInterface $runner
+     */
+    public function __construct(RunnerInterface $runner, EngineInterface $templating)
     {
-        return $this->render('ConnectHollandHealthCheckBundle:Default:index.html.twig');
+        $this->runner = $runner;
+        $this->templating = $templating;
+        $this->suites = [];
+    }
+
+    /**
+     * Performs health checks and returns Response container results
+     *
+     * @return Response
+     */
+    public function healthAction()
+    {
+        $this->runner->run();
+        $result = $this->getResults();
+        return new Response(
+            $this->templating->render(
+                'ConnectHollandHealthCheckBundle:health.html.twig',
+                ['result' => $result]
+            )
+        );
+    }
+
+    /**
+     * Registers a completed suite
+     *
+     * @param SuiteInterface $suite
+     */
+    public function registerSuiteResult(SuiteInterface $suite)
+    {
+        $this->suites[] = $suite;
+    }
+
+    /**
+     * Rewrite the suites into a result array
+     *
+     * @return array
+     */
+    private function getResults()
+    {
+        $result = [];
+        foreach ($this->suites as $suite) {
+            $fqCls = get_class($suite);
+            $suiteName = substr($fqCls, strrpos($fqCls, '\\') + 1);
+            $result[$suiteName] = [];
+
+            $testResults = $suite->getResults();
+            foreach ($testResults as $testResult) {
+                $testCls = get_class($testResult->getTest());
+                $testName = substr($testCls, strrpos($testCls, '\\') + 1);
+                $result[$suiteName][] = ['test' => $testName, 'result' => $testResult->get()];
+            }
+        }
+
+        return $result;
     }
 }
